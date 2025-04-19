29a7646d057b089df6f9f6c9b3971d6ab3d9dcd0
BriTech-Nova <kodenova.tech@gmail.com>
2025-04-20 00:23:14 +0300

    testing contact form

--- Megamotive_inc/mega_motive/views.py
+++ Megamotive_inc/mega_motive/views.py
@@ -3,31 +3,23 @@ from django.contrib import messages
 from .forms import ContactForm
 
 def home(request):
-    form = ContactForm()
-    
+    """
+    Home view that also handles contact form submissions.
+    """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your message has been sent successfully!')
-            return redirect('home')
+            return redirect('home')  # Changed to 'home' assuming this is the correct URL name
+        else:
+            messages.error(request, 'There was an error sending your message. Please try again.')
+    else:
+        form = ContactForm()
     
     context = {
         'form': form
     }
     return render(request, 'mega_motive/index.html', context)
 
-def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            # Save the form data to the database
-            form.save()
-            messages.success(request, 'Your message has been sent successfully!')
-            return redirect('index')  # Replace 'index' with the name of your homepage URL
-        else:
-            messages.error(request, 'There was an error sending your message. Please try again.')
-    else:
-        form = ContactForm()
-
-    return render(request, 'mega_motive/index.html', {'form': form})
+# The contact_view function has been removed as it duplicated functionality
