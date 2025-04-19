792c49ebad0f6fa4f6004c09b435ecbf0fcdb8d4
BriTech-Nova <kodenova.tech@gmail.com>
2025-04-19 22:28:51 +0300

    database migrations

--- Megamotive_inc/Megamotive_inc/settings.py
+++ Megamotive_inc/Megamotive_inc/settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project_name.urls'
+ROOT_URLCONF = 'Megamotive_inc.urls'
 
 TEMPLATES = [
     {
@@ -51,7 +51,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project_name.wsgi.application'
+WSGI_APPLICATION = 'Megamotive_inc.wsgi.application'
 
 # Database
 DATABASES = {
