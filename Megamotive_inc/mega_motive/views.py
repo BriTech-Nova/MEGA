d5381db90fed187ee1fbcbdfa34384a021c40080
BriTech-Nova <kodenova.tech@gmail.com>
2025-04-19 23:53:34 +0300

    send us message form handler

--- Megamotive_inc/mega_motive/views.py
+++ Megamotive_inc/mega_motive/views.py
@@ -16,3 +16,18 @@ def home(request):
         'form': form
     }
     return render(request, 'mega_motive/index.html', context)
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Save the form data to the database
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('index')  # Replace 'index' with the name of your homepage URL
+        else:
+            messages.error(request, 'There was an error sending your message. Please try again.')
+    else:
+        form = ContactForm()
+
+    return render(request, 'mega_motive/index.html', {'form': form})
